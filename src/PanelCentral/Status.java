e6b1a7a3f277f4b9ead26ff64e7ae1588a92ec1f
TomoCid <tocid2021@inf.udec.cl>
2022-11-22 20:26:35 -0300

    Arreglo commit anterior

--- src/PanelCentral/Status.java
+++ src/PanelCentral/Status.java
@@ -6,96 +6,83 @@ import java.awt.Graphics;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Status extends JPanel implements ActionListener{
-    
+public class Status extends JPanel implements ActionListener {
+
     //Creamos un panel contenedor, un panel Menu y un panel InGame (Este ultimo es una clase)
-    JPanel Panels,Menu;
-    JButton Iniciar,Salir;
+    JPanel Panels, Menu;
+    JButton Iniciar, Salir;
     JFrame target;
     CardLayout c1 = new CardLayout();
 
-    
-    
     InGame enjuego = new InGame(); //Panel in game
 
+    boolean appIsRunning = false;
 
-    boolean appIsRunning=false;
-
-
-    public Status(JFrame target){
+    public Status(JFrame target) {
 
         this.target = target;
-        
+
         InitPanels();
         InitMenuComponents();
         InitRunningComponents();
 
         target.add(Panels);
-        
 
     }
-    
-    
-    public void InitPanels(){
+
+    public void InitPanels() {
         //Panel contenedor, se utiliza CardLayout para hacer cambio entre los paneles internos.
         Panels = new JPanel();
         Panels.setLayout(c1);
-        
+
         //Creamos los paneles de Menu y ProgramaIniciado
         Menu = new JPanel();
         Menu.setLayout(null);
 
         //Se agregan estos paneles al panel contenedor
-        Panels.add(Menu,"1");
-        Panels.add(enjuego,"2");
+        Panels.add(Menu, "1");
+        Panels.add(enjuego, "2");
 
     }
-   
-    
 
-    
-    public void InitMenuComponents(){
+    public void InitMenuComponents() {
         //Creamos los botones del menu
         Iniciar = new JButton();
-        Iniciar.setBounds(350,100,300,50);
+        Iniciar.setBounds(350, 100, 300, 50);
         Iniciar.addActionListener(this);
         Menu.add(Iniciar);
-        
+
         Salir = new JButton();
-        Salir.setBounds(350,200,300,50);
+        Salir.setBounds(350, 200, 300, 50);
         Salir.addActionListener(this);
         Menu.add(Salir);
 
     }
-    
-    public void InitRunningComponents(){
+
+    public void InitRunningComponents() {
         //Creamos todos los elementos del panel ProgramaIniciado
         JLabel test = new JLabel();
         test.setOpaque(true);
         test.setBackground(Color.red);
-        test.setBounds(100,100,50,50);
+        test.setBounds(100, 100, 50, 50);
         enjuego.add(test);
-        
-    }
-    
-    
 
-    
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e){
-        
-        if(e.getSource()==Iniciar){
-          //Cambiamos al panel ProgramaIniciado
-          c1.show(Panels, "2");
-          enjuego.requestFocus();
-          appIsRunning=true;
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == Iniciar) {
+            //Cambiamos al panel ProgramaIniciado
+            c1.show(Panels, "2");
+            enjuego.requestFocus();
+            appIsRunning = true;
 
         }
-        if(e.getSource()==Salir){
+        if (e.getSource() == Salir) {
             System.exit(0);
         }
-          
+
     }
-     
-    
+
 }
