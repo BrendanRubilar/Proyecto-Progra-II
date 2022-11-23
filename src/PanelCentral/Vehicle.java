a0b66ce68f40b7f340ccd5a4fbe677ded4d8482c
TomoCid <tocid2021@inf.udec.cl>
2022-11-22 23:01:41 -0300

    Pruebas con giro de ruedas (Faltan ajustes)

--- src/PanelCentral/Vehicle.java
+++ src/PanelCentral/Vehicle.java
@@ -6,18 +6,37 @@ public class Vehicle {
 
     private int X = 250, Y = 195, H = 80, W = 60;
     private double  giro = 0;
-    private Color colorAuto;
 
     public Vehicle() {
 
     }
 
     public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D car = (Graphics2D) g;
+        Graphics2D wheel1 = (Graphics2D) g;
+        Graphics2D wheel2 = (Graphics2D) g;
+        Graphics2D wheel3 = (Graphics2D) g;
+        Graphics2D wheel4 = (Graphics2D) g;
 
-        g2d.rotate(Math.toRadians(giro), X + 30, Y);
-        g2d.setColor(Color.yellow);
-        g2d.fillRect(X, Y, W, H);
+        wheel1.rotate(Math.toRadians(giro), X+30, Y+10);
+        wheel1.setColor(Color.BLACK);
+        wheel1.fillRect(X-5, Y+10, W-50, H-60);
+
+        wheel2.rotate(Math.toRadians(giro), X+30, Y+10);
+        wheel2.setColor(Color.BLACK);
+        wheel2.fillRect(X+55, Y+10, W-50, H-60);
+
+        wheel3.rotate(Math.toRadians(-giro), X+30, Y+50);
+        wheel3.setColor(Color.BLACK);
+        wheel3.fillRect(X-5, Y+50, W-50, H-60);
+
+        wheel4.rotate(Math.toRadians(-giro), X+30, Y+50);
+        wheel4.setColor(Color.BLACK);
+        wheel4.fillRect(X+55, Y+50, W-50, H-60);
+
+        car.rotate(Math.toRadians(-giro), X + 30, Y+80);
+        car.setColor(Color.yellow);
+        car.fillRect(X, Y, W, H);
 
     }
 
@@ -32,28 +51,45 @@ public class Vehicle {
     }
     public void MoveUp() {
         if (Y >= 20) {
-            Y = Y - 5;
+            Y = Y - 8;
+            if(giro>0.4){
+                giro=giro-0.5;
+            }else if(giro<-0.4){
+                giro=giro+0.5;
+            }else{
+                giro=0;
+            }
         }
     }
 
     public void MoveRight() {
         if (X <= 1000) {
-             X = X + 5;
-            giro = giro - 0.175;
+             X = X + 8;
+             if(giro>=-5){
+                giro = giro - 0.4;
+            }
         }
     }
 
     public void MoveLeft() {
         if (X >= 20) {
-            X = X - 5;
-            giro = giro + 0.175;
-
+            X = X - 8;
+            if(giro<=5){
+                giro = giro + 0.4;
+            }
         }
     }
 
     public void MoveDown() {
         if (Y <= 600) {
-            Y = Y + 5;
+            Y = Y + 8;
+            if(giro>0.4){
+                giro=giro-0.5;
+            }else if(giro<-0.4){
+                giro=giro+0.5;
+            }else{
+                giro=0;
+            }
         }
 
     }
